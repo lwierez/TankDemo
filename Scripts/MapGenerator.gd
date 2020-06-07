b3c60a5f4c3437f96490b2923472ccc81b28c182
WIEREZ Loïc <loic.wierez@viacesi.fr>
2020-06-07 18:24:30 +0200

    [Files] Updated terrain material

--- Scripts/MapGenerator.gd
+++ Scripts/MapGenerator.gd
@@ -19,35 +19,35 @@ func _ready():
 		for z in range(height-1):
 			#A
 			st.add_normal(Vector3(0, 1, 0))
-			st.add_uv(Vector2(0, 0))
+			st.add_uv(Vector2(x, z))
 			st.add_vertex(Vector3(x, noise_texture_data.get_pixel(x, z).r * 10, z))
 			
 			#B
 			st.add_normal(Vector3(0, 1, 0))
-			st.add_uv(Vector2(0, 0))
+			st.add_uv(Vector2(x+1, z))
 			st.add_vertex(Vector3(x+1, noise_texture_data.get_pixel(x+1, z).r * 10, z))
 		
 			#C
 			st.add_normal(Vector3(0, 1, 0))
-			st.add_uv(Vector2(0, 0))
+			st.add_uv(Vector2(x, z+1))
 			st.add_vertex(Vector3(x, noise_texture_data.get_pixel(x, z+1).r * 10, z+1))
 			
 			#B
 			st.add_normal(Vector3(0, 1, 0))
-			st.add_uv(Vector2(0, 0))
+			st.add_uv(Vector2(x+1, z))
 			st.add_vertex(Vector3(x+1, noise_texture_data.get_pixel(x+1, z).r * 10, z))
 			
 			#D
 			st.add_normal(Vector3(0, 1, 0))
-			st.add_uv(Vector2(0, 0))
+			st.add_uv(Vector2(x+1, z+1))
 			st.add_vertex(Vector3(x+1, noise_texture_data.get_pixel(x+1, z+1).r * 10, z+1))
 			
 			#C
 			st.add_normal(Vector3(0, 1, 0))
-			st.add_uv(Vector2(0, 0))
+			st.add_uv(Vector2(x, z+1))
 			st.add_vertex(Vector3(x, noise_texture_data.get_pixel(x, z+1).r * 10, z+1))
 
 	var mesh = st.commit()
 	$StaticBody/MeshInstance.mesh = mesh
-	$StaticBody/MeshInstance.material_override = load("res://Materials/Materials/terrain.tres")
+	$StaticBody/MeshInstance.material_override = load("res://Materials/Materials/terrain.material")
 	$StaticBody/CollisionShape.shape = mesh.create_trimesh_shape()
